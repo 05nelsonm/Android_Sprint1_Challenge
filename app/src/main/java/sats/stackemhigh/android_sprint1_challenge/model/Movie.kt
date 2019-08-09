6411e18e2799d04c90b097a6e0673dd8bda3db56
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-09 11:18:12 -0400

    organized and setup file structure

---
+++ app/src/main/java/sats/stackemhigh/android_sprint1_challenge/model/Movie.kt
@@ -0,0 +1,4 @@
+package sats.stackemhigh.android_sprint1_challenge.model
+
+class Movie {
+}
---
+++ app/src/main/java/sats/stackemhigh/android_sprint1_challenge/ui/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package sats.stackemhigh.android_sprint1_challenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sats.stackemhigh.android_sprint1_challenge.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
